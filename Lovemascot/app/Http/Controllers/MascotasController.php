85b29715ac02a2a4030166d0ad1e5abf2a8e3c0d
USIS051620 <marvinjosuesolorzano@gmail.com>
2022-04-25 21:32:21 -0600

    Inicio del Proyecto Lovemascot

---
+++ Lovemascot/app/Http/Controllers/MascotasController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mascotas;
+use Illuminate\Http\Request;
+
+class MascotasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // return view('mascotas');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $parametros = $this->validar($request)->only(['nombre', 'raza', 'color', 'edad', 'sexo',]);
+        $mascota = Mascotas::create($parametros);
+        return response()->json(['id' => $mascota->id]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Mascotas  $mascotas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Mascotas $mascotas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Mascotas  $mascotas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Mascotas $mascotas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Mascotas  $mascotas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Mascotas $mascotas)
+    {
+        // Si el campo cartilla no esta vacio, se actualizan los demas campos
+        if ($request->cartilla != '') {
+            $parametros = $request->validate([
+                'cartilla' => 'required|string|',
+            ]);
+            $imagen = $request->imagen;
+            $imagen = explode(',', $imagen);
+            $imagenNombre = $request->id_mascota.'.png';
+            $path = 'storage/imagenes/mascotas/cartillas/'.$imagenNombre;
+            file_put_contents($path, base64_decode($imagen[1]));
+            $mascota = Mascotas::find($request->id_mascota);
+            $mascota->cartilla = $imagenNombre;
+            $mascota->save();
+            return response()->json(['status' => 'success']);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Mascotas  $mascotas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Mascotas $mascotas)
+    {
+        //
+    }
+
+    public function cartilla(Request $request)
+    {
+        $parametros = $request->validate([
+            'imagen' => 'required|string|',
+        ]);
+        $imagen = $request->imagen;
+        $imagen = explode(',', $imagen);
+        $imagenNombre = $request->id_mascota.'.png';
+        $path = 'storage/imagenes/mascotas/cartillas/'.$imagenNombre;
+        file_put_contents($path, base64_decode($imagen[1]));
+        $mascota = Mascotas::find($request->id_mascota);
+        $mascota->cartilla = $imagenNombre;
+        $mascota->save();
+        return response()->json(['status' => 'success']);
+    }
+
+    public function validar($parameters)
+    {
+        $this->validate($parameters, [
+            'nombre' => 'required|string|max:255',
+            'raza' => 'required|string|max:255',
+            'color' => 'required|string|max:255',
+            'edad' => 'required|integer|min:1',
+            'sexo' => 'required|string|max:255',
+        ]);
+        return $parameters;
+    }
+}
