51adf93390a02e50a8c8aa7f6c6256f0ef532dbb
Cesia Flores09 <florescsia@gmail.com>
2022-05-18 22:55:17 -0600

    Cambios inicios de node y express

--- sistema_academico/server/index.js
+++ sistema_academico/server/index.js
@@ -1,16 +1,18 @@
-const express = require('express');
-      server = express(),
-      port = 3001;
+const express = require('express'),
+    server = express(),
+    port = 3001;
 
 server.use(express.json());
-server.use(express.cors());
+
 server.get('/chat', function(req, resp){
     resp.send('Hola mundo');
 
 });
+
 server.get('/usuarios', function(req, resp){
+    console.log(req.body);
     resp.send('Bienvenidos al sitio de chats');
-});   
+});
 
 server.listen(port, function(event){
     console.log(`Server running on port ${port}`);
