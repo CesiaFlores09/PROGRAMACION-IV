037f1c2082ba25ebed6c3314f9c79933b300c114
Cesia Flores09 <florescsia@gmail.com>
2022-05-19 14:28:47 -0600

    Mongo+nodejs+ express

--- sistema_academico/server/index.js
+++ sistema_academico/server/index.js
@@ -1,5 +1,7 @@
 const express = require('express'),
     server = express(),
+    mongodb= require('mongodb').MongoClient,
+    url = 'mongodb://localhost:27017',
     port = 3001;
 
 server.use(express.json());
@@ -9,9 +11,33 @@ server.get('/chat', function(req, resp){
 
 });
 
-server.get('/usuarios', function(req, resp){
-    console.log(req.body);
-    resp.send('Bienvenidos al sitio de chats');
+server.get('/usuarios/list', function(req, resp){
+    mongodb.connect(url, function(err, client){
+        if(err) resp.send(err);
+        const db = client.db('prue');
+        db.collection('usuarios').find({}).toArray(function (err, data){
+            if(err) resp.send(err);
+            resp.send(data);
+
+
+        });
+    });
+   
+});
+server.get('/usuarios/nuevo', function(req, resp){
+    resp.sendFile(__dirname + '/index.html');
+});
+server.post('usuarios/save', function(resq, resp){
+    mongodb.connect(url, function(err, client){
+      if(err) resp.send(err);
+      const db = client.db('prue');
+      db.collection('usuarios').insertOne(req.body, function(err, result){
+          if(err) resp.send(err);
+          resp.send(result);
+
+      });
+
+    });
 });
 
 server.listen(port, function(event){
